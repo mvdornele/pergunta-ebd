3fb501dd62d623c5a0e4932486a39ece5007ae93
mvdornele <61663575+mvdornele@users.noreply.github.com>
2025-04-30 18:07:52 -0300

    Pergunta EBD quatro de maio

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ CORS(app)
 
 pergunta = {
     "pergunta": "Lendo Mateus, capítulo 13, verso 8, e Apocalipse, capítulo 2, verso 7, qual a promessa que o Senhor fez para aqueles que receberam a Palavra de Deus e deram frutos? ",
-    "opcoes": ["Eles comerão do fruto da árvore da vida, que está no meio do paraíso de Deus.", "Eles NÃO comerão do fruto da árvore da vida, que está no meio do paraíso de Deus.", "Nenhuma das alternativas"]
+    "opcoes": ["Eles comerão do fruto da árvore da vida, que está no meio do paraíso de Deus.", "Eles NÃO comerão do fruto da árvore da vida, que está no meio do paraíso de Deus."]
 }
 
 @app.route("/pergunta")
