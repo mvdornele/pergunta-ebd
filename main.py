497a819f272607e24883657d3b26beeb783955eb
mvdornele <61663575+mvdornele@users.noreply.github.com>
2025-06-02 21:18:22 -0300

    Perguntas EBD 08/01

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ def obter_perguntas():
         "perguntas": {
             "para_todos": [
                 {
-                    "texto": "2.Lendo Apocalipse, capítulo 3, verso 9, comparado Apocalipse, capítulo 2, verso 16, qual o recurso que o Senhor colocou na igreja em Filadelfia para combater a oposição do anátema?",
+                    "texto": "1.Lendo Apocalipse, capítulo 3, versos de 7 a 13, identifique as sete operações do Espírito de Deus em Filadélfia. Escolha um breve comentário sobre: A) Espírito do Senhor B) Espírito de Sabedoria C) Espírito de Inteligência",
                     "opcoes": [
-                        "A revelação da Palavra pelo Espírito Santo, que é o ensino verdadeiro vindo da boca do Senhor.",
-                        "A doutrina pura transmitida pela espada do Espírito, que é a Palavra de Deus revelada à igreja fiel.",
-                        "A espada da boca do Senhor, a Palavra revelada."
+                        "A) Espírito do Senhor (Essa operação revela a autoridade suprema de Jesus sobre a Igreja. Ele governa, abre e fecha portas espirituais, e conduz a Igreja segundo sua soberania).",
+                        "B) Espírito de Sabedoria (A igreja de Filadélfia agiu com sabedoria ao valorizar a Palavra e o Nome do Senhor, mesmo em tempos de perseguição. A sabedoria do alto os fez entender o que realmente tinha valor espiritual).",
+                        "C) Espírito de Inteligência ( A inteligência espiritual capacita o crente a discernir a vontade de Deus e agir com entendimento. Filadélfia entendeu o significado da "porta aberta" — uma oportunidade de serviço, comunhão e salvação — e seguiu com fé."
                     ]
                 }
             ],
             "para_cias": [
                 {
-                    "texto": "6.No livro de Apocalipse, capítulo 3, verso 8, fala sobre uma porta aberta. No livro de João,capítulo 10, verso 9, quem é essa porta que nos salva?",
+                    "texto": "3.Lendo Mateus, capítulo 13, versos 45 e 46, como a pérola de grande valor nos ajuda a entender o que o Espírito Santo nos mostra sobre Jesus?",
                     "opcoes": [
-                        "Jesus é a porta pela qual entramos e somos salvos; quem entra por Ele encontra salvação, segurança e pastagem.",
-                        "A porta é o próprio Senhor Jesus, que nos conduz à salvação e à vida eterna.",
-                        "Jesus é a porta aberta. Ele nos salva e cuida de nós."
+                        "O negociante achou uma pérola muito especial e ficou com ela. A igreja de Filadélfia entendeu que Jesus é muito especial. O Espírito Santo mostra que Jesus é precioso, como essa pérola.",
+                        "Assim como o negociante vendeu tudo para adquirir a pérola, o Espírito Santo nos mostra que Jesus é tão valioso que vale a pena deixar tudo para segui-Lo.",
+                        "A pérola é única, rara e perfeita — assim como Jesus. O Espírito Santo nos revela essa beleza e valor espiritual de Cristo, mostrando que Ele é o tesouro maior que Deus nos deu."
                     ]
                 }
             ]
