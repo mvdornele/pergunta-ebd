2da976fafa6b7f0d48718d808080292c57efe836
mvdornele <61663575+mvdornele@users.noreply.github.com>
2025-08-05 07:25:58 -0300

    ebd 10/08

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ def obter_perguntas():
         "perguntas": {
             "para_todos": [
                 {
-                    "texto": "3.Lendo Apocalipse, capítulo 3, verso 20, o que temos que fazer para o Senhor Jesus habitar em nosso coração?",
+                    "texto": "1. Identifique a relação profética entre Apocalipse, capítulo 3, verso 16, e Mateus, capítulo 13,verso 48, quanto à operação do Espírito de Temor na igreja de Laodiceia",
                     "opcoes": [
-                        "Ouvir a voz do Senhor Jesus e abrir o coração para Ele entrar e fazer morada em nossa vida.",    
-                        "Estar atentos à voz do Senhor e responder ao Seu chamado com obediência e fé, permitindo que Ele transforme nosso interior.",
-                        "Receber Jesus com sinceridade, entregando nossa vida a Ele e permitindo que Sua presença guie nossos pensamentos e atitudes."
+                        "A relação profética está em uma igreja lançada fora do corpo, tal como os peixes ruins lançados fora do cesto. Uma igreja que perdeu o temor do Senhor. ",    
+                        "A igreja de Laodiceia representa um grupo que perdeu o temor do Senhor, tornando-se indiferente espiritualmente, como os peixes ruins que são rejeitados no juízo final.",
+                        "Assim como os peixes ruins foram lançados fora por não servirem ao propósito, a igreja morna de Laodiceia é rejeitada por ter perdido o zelo e o temor diante de Deus."
                     ]
                 }
             ],
             "para_cias": [
                 {
-                    "texto": "4.Lendo Apocalipse, capítulo 3, verso 20, o que temos que fazer para Jesus morar no nosso coração?",
+                    "texto": "3. Leia Provérbios, capítulo 1, verso 7 e Eclesiastes, capítulo 12, verso 13, e responda: O que o servo de Deus deve ter no coração desde o começo até o fim da sua vida?",
                     "opcoes": [
-                        "Estar atentos à voz do Senhor e responder ao Seu chamado com obediência e fé, permitindo que Ele transforme nosso interior.",
-                        "Ouvir Jesus. Abrir o coração para Jesus entrar e morar",
-                        "Receber Jesus com sinceridade, entregando nossa vida a Ele e permitindo que Sua presença guie nossos pensamentos e atitudes."
+                        "O temor do Senhor deve estar presente em toda a caminhada do servo de Deus, desde o início até o fim.",
+                        "O servo de Deus deve ter o temor do Senhor no coração.",
+                        "Desde o começo da sua vida até o fim, o servo de Deus deve guardar no coração o temor e a obediência ao Senhor."
                     ]
                 }
             ]
