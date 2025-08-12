d78a063e7ed654ce45fc7e10634e74bd9d253cdd
mvdornele <61663575+mvdornele@users.noreply.github.com>
2025-08-12 07:27:34 -0300

    ebd 17/08

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ def obter_perguntas():
         "perguntas": {
             "para_todos": [
                 {
-                    "texto": "1. Identifique a relação profética entre Apocalipse, capítulo 3, verso 16, e Mateus, capítulo 13,verso 48, quanto à operação do Espírito de Temor na igreja de Laodiceia",
+                    "texto": "1. Lendo Apocalipse, capítulo 3, versos 17 e 18, identifique a relação entre as seguintes características da igreja de Laodiceia e os conselhos do Senhor Jesus. Faça a aplicação profética",
                     "opcoes": [
-                        "A relação profética está em uma igreja lançada fora do corpo, tal como os peixes ruins lançados fora do cesto. Uma igreja que perdeu o temor do Senhor. ",    
-                        "A igreja de Laodiceia representa um grupo que perdeu o temor do Senhor, tornando-se indiferente espiritualmente, como os peixes ruins que são rejeitados no juízo final.",
-                        "Assim como os peixes ruins foram lançados fora por não servirem ao propósito, a igreja morna de Laodiceia é rejeitada por ter perdido o zelo e o temor diante de Deus."
+                        "Batismo com o Espírito Santo, discernimento espiritual e experiência de salvação — a igreja de Laodiceia carece desses três aspectos, vivendo na força humana, sem santidade e misturada com o mundo.",    
+                        "Falta à igreja de Laodiceia poder espiritual, clareza para distinguir o santo do profano e vida transformada pelo sangue de Jesus.",
+                        "A igreja de Laodiceia vive sem o poder do Espírito Santo, sem discernimento e sem comunhão verdadeira com Deus, conformando-se com o mundo."
                     ]
                 }
             ],
             "para_cias": [
                 {
-                    "texto": "3. Leia Provérbios, capítulo 1, verso 7 e Eclesiastes, capítulo 12, verso 13, e responda: O que o servo de Deus deve ter no coração desde o começo até o fim da sua vida?",
+                    "texto": "2. Leia 1 Samuel, capítulo 16, verso 7b e Atos, capítulo 13, verso 22b, e responda: Por que Deus escolheu Davi para ser rei de Israel?",
                     "opcoes": [
-                        "O temor do Senhor deve estar presente em toda a caminhada do servo de Deus, desde o início até o fim.",
-                        "O servo de Deus deve ter o temor do Senhor no coração.",
-                        "Desde o começo da sua vida até o fim, o servo de Deus deve guardar no coração o temor e a obediência ao Senhor."
+                        "Porque Davi tinha um coração obediente a Deus.",
+                        "Porque Davi tinha um coração segundo o coração de Deus.",
+                        "Porque Davi fazia a vontade de Deus em todas as coisas."
                     ]
                 }
             ]
