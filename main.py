05905afed1a00c059a992eb7c0a3ac2a7b5786e0
mvdornele <61663575+mvdornele@users.noreply.github.com>
2025-09-10 11:18:40 -0300

    EBD 14-09

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ def obter_perguntas():
         "perguntas": {
             "para_todos": [
                 {
-                    "texto": "1. Ao ler Apocalipse, capítulo 4, verso 7, identifique o significado profético das seguintes designações atribuidas ao Senhor Jesus na vidão de João- A- Semelhante a um Leão B- semelhamte a um Bezerro C- O rosto como Homem D-Semelhante a uma águia voando.",
+                    "texto": "1.Ao ler Apocalipse, Capitulo 5, versos 5 e 6, identifique a relação profética entre três designações atribuidas ao senhor jesus na visão de João. Faça aplicação na vida da Igreja.,
                     "opcoes": [
-                        "Leão → representa o Senhor Jesus como Rei soberano, aquele que reina com poder, autoridade e majestade sobre toda a criação.",    
-                        "Bezerro → aponta para o sacrifício perfeito de Cristo, que se entregou em favor da Igreja, revelando obediência e serviço.",
-                        "Homem → mostra o Jesus encarnado, que viveu como homem e se compadece das nossas fraquezas, sendo o intercessor fiel."
+                        "O Leão da tribo de Juda: Mostra que Jesus é o Rei vitorioso, que reina sobre todas as coisas e dá à igreja segurança diante das lutas espirituais.",    
+                        "A raiz de Davi: Ensina que toda a esperança e governo espiritual da igreja estão firmados em Cristo, o verdadeiro Rei prometido, que sustenta e conduz o Seu povo.",
+                        "O Cordeiro, como havendo sido morto: Mostra que a vitória da igreja está no sangue do Cordeiro. Ele é a base da nossa salvação, adoração e comunhão eterna com Deus."
                     ]
                 }
             ],
             "para_cias": [
                 {
-                    "texto": "2. Leia Hebreus, capítulo 11, verso 23 e responda, O que levou os pais de Moisés a não temerem faraó quando Moisés nasceu?.",
+                    "texto": "2.Leia Êxodo, capítulo 3, verso 4, e responda: O que Moisés respondeu quando Deus o chamou?.",
                     "opcoes": [
-                        "Eles viram que Moisés era um menino formoso, separado por Deus, e reconheceram que havia um propósito especial na vida dele.",
-                        "Agiram pela fé, confiando que Deus tinha um plano maior, e por isso não temeram o decreto de Faraó.",
-                        "Tiveram coragem e confiança no Senhor, pois creram que a proteção divina era maior que a ordem do rei"
+                        "Disponibilidade – Moisés mostrou-se pronto para ouvir a voz de Deus.",
+                        "Entrega – Ao dizer “Eis-me aqui”, ele reconheceu que sua vida estava nas mãos do Senhor.",
+                        "Chamado – Foi o início de sua missão como libertador do povo de Israel."
                     ]
                 }
             ]
